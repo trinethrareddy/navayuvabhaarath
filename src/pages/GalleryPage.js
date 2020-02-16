c52d28068d71d005dd9758c45cca1b04ecd95134
muni <muni2intell@gmail.com>
2020-02-16 21:04:22 +0530

    added imagr react grid

--- src/pages/GalleryPage.js
+++ src/pages/GalleryPage.js
@@ -2,11 +2,15 @@ import React, {Component} from 'react';
 
 import SmallBannerComponent from '../components/SmallBanner';
 import subscribeImg from '../assets/images/subscribe.jpg';
+import ImageGalleryGrid from '../components/Image-gallery-grid';
+import Title from '../components/Title';
 class GaleryPage extends Component {
     render() {
         return (
             <div>
                 <SmallBannerComponent bgImage={subscribeImg} bannerText="NavaYuva Bharath Gallery" />
+                <Title titleName="Image Gallery"></Title>
+                <ImageGalleryGrid/>
             </div>
         )
     }
