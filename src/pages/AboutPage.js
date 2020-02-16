4a501f3a54fdf5e047c8dcd361665242e619a05e
muni <muni2intell@gmail.com>
2020-02-16 20:18:19 +0530

    added gallery component in aboutus page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 import SmallBannerComponent from '../components/SmallBanner';
 import subscribeImg from '../assets/images/subscribe.jpg';
+import GalleryComponent from '../components/Gallery';
 class AboutPage extends Component {
   render() {
     return (
+      <div>
       <SmallBannerComponent bgImage={subscribeImg} bannerText="About Us NavaYuva Bharath"/>
+        <GalleryComponent/>
+      </div>
+      
     )
   }
 }
